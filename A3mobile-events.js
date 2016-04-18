1bac26b60805c998767c5d0d09e6e0420c5d157d
A3mobile <a3mobile@a3mobile.co>
2016-04-18 14:25:51 -0700

    A3mobile-events
    A3mobile-events

---
+++ A3mobile-events.js
@@ -0,0 +1,345 @@
+/***********************************************/
+/*
+/*
+/*		A3mobile
+/*
+/*		version 1.0.0
+/*		https://a3mobile.co/code/A3mobile-events-1.0.0.js
+/*
+/*		Copyright 2016
+/*		https://a3mobile.co
+/*
+/*		require jQuery.js
+/*
+/*
+/***********************************************/
+
+function prevWindowUrl(){
+	return $.data(window,"prevWindowUrl");
+};
+function currentWindowUrl(){
+	return $.data(window,"currentWindowUrl");
+};
+(function(){
+	$.data(window,"prevWindowUrl",window.location.href);
+	$.data(window,"currentWindowUrl",window.location.href);
+	setInterval(function(){
+		if(currentWindowUrl()!==window.location.href){
+			$.data(window,"prevWindowUrl",currentWindowUrl());
+			$.data(window,"currentWindowUrl",window.location.href);
+			$.event.dispatch.call(window,"urlChange");
+		}
+	},100);
+	$.event.special.orientationEvents={
+		setup:function(){
+			$(window).on('orientationchange',function(event){
+				triggercustomevent(window,"orientationChange",event);
+				switch(window.orientation){
+					case 0:
+						triggercustomevent(window,"portraitNormal",event);
+						triggercustomevent(window,"portrait",event);
+					break;
+					case 90:
+						triggercustomevent(window,"landscapeLeft",event);
+						triggercustomevent(window,"Landscape",event);
+					break;
+					case -90:
+						triggercustomevent(window,"landscapeRight",event);
+						triggercustomevent(window,"landscape",event);
+					break;
+					case 180:
+						triggercustomevent(window,"portraitUpDown",event);
+						triggercustomevent(window,"portrait",event);
+					break;
+				}
+			});
+		}
+	};
+	$.event.special.tapEvents={
+		setup:function(){
+			var touch_object,
+			tap_time,
+			this_jq=$(this),
+			this_object=this,
+			tap_intval,
+			touch_x=[],
+			touch_y=[],
+			is_canceled=false,
+			is_taphold=false;
+			this_jq.bind("touchstart",function(event){
+				touch_object=event.target;
+				var touches=event.originalEvent.changedTouches,
+				current_time;
+				if(touches.length===2){
+					touch_x[0]=touches[0].pageX;
+					touch_y[0]=touches[0].pageY;
+					touch_x[1]=touches[1].pageX;
+					touch_y[1]=touches[1].pageY;
+				}else{
+					touch_x[0]=touches[0].pageX;
+					touch_y[0]=touches[0].pageY;
+				}
+				tap_time=Date.now();
+				is_canceled=false;
+				
+				tap_intval=setInterval(function(){
+					current_time=Date.now();
+					if(current_time>tap_time+750 && !is_canceled && touches.length===1){
+						is_taphold=true;
+						clearInterval(tap_intval);
+						triggercustomevent(this_object,"tapHold",event);
+					}
+				},50);
+			});
+			this_jq.bind("touchmove",function(event){
+				is_canceled=true;
+			});
+			this_jq.bind("touchend",function(event){
+				if(is_canceled){
+					return;
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				clearInterval(tap_intval);
+				var timer=Date.now(),touches=event.originalEvent.changedTouches;
+				if(!is_taphold && !is_canceled && touch_object===event.target && touches.length===1 && touch_x[0]===touches[0].pageX && touch_y[0]===touches[0].pageY){
+					triggercustomevent(this_object,"tap",event);
+				}
+				if(!is_taphold && !is_canceled && touch_object===event.target && touches.length===2 && touch_x[0]===touches[0].pageX && touch_y[0]===touches[0].pageY && touch_x[1]===touches[1].pageX && touch_y[1]===touches[1].pageY){
+					triggercustomevent(this_object,"tapTwo",event);
+				}
+				is_taphold=false;
+				is_canceled=true;
+				event.preventDefault();
+				event.stopPropagation();
+			});
+		},
+		teardown:function(){
+			$(this).unbind("touchstart").unbind("touchmove").unbind("touchend");
+		}
+	};
+	$.event.special.swipeEvents={
+		setup:function(){
+			var touch_object,
+			this_jq=$(this),
+			this_object=this,
+			is_swipe=false,
+			touch_x=[],
+			touch_y=[],
+			touch_object_x,
+			touch_object_y,
+			touch_object_width,
+			touch_object_height;
+			this_jq.on("touchstart",function(event){
+				touch_object=event.target;
+				var touches=event.originalEvent.changedTouches;
+				if(touches.length===2){
+					touch_x[0]=touches[0].pageX;
+					touch_y[0]=touches[0].pageY;
+					touch_x[1]=touches[1].pageX;
+					touch_y[1]=touches[1].pageY;
+				}else{
+					touch_x[0]=touches[0].pageX;
+					touch_y[0]=touches[0].pageY;
+				}
+				touch_object_x=$(this).offset().left;
+				touch_object_y=$(this).offset().top;
+				touch_object_width=$(this).outerWidth();;
+				touch_object_height=$(this).outerHeight();;
+				is_swipe=true;
+			});
+			this_jq.bind("touchmove",function(event){
+				var current_object=event.target,
+				touches=event.originalEvent.changedTouches,
+				vertical_scrolled=$(this).outerHeight() < this.scrollHeight,
+				horizontal_scrolled=$(this).outerWidth() < this.scrollWidth;
+				if(is_swipe && current_object===touch_object && touches){
+					if(touches.length===1 && (touch_object_x < touches[0].pageX) && (touch_object_y < touches[0].pageY) && ((touch_object_x + touch_object_width) > touches[0].pageX) && ((touch_object_y + touch_object_height) > touches[0].pageY)){
+						if((touches[0].pageX > (touch_x[0]+50)) && (touches[0].pageY > touch_y[0]-30) && (touches[0].pageY < touch_y[0]+30)){
+							if(horizontal_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeRight",event);
+						}
+						if((touches[0].pageX < (touch_x[0]-50)) && (touches[0].pageY > touch_y[0]-30) && (touches[0].pageY < touch_y[0]+30)){
+							if(horizontal_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeLeft",event);
+						}
+						if((touches[0].pageY > (touch_y[0]+50)) && (touches[0].pageX > touch_x[0]-30) && (touches[0].pageX < touch_x[0]+30)){
+							if(vertical_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeDown",event);
+						}
+						if((touches[0].pageY < (touch_y[0]-50)) && (touches[0].pageX > touch_x[0]-30) && (touches[0].pageX < touch_x[0]+30)){
+							if(vertical_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeUp",event);
+						}
+					}
+					if(touches.length===2 && 
+					(touch_object_x < touches[0].pageX) && (touch_object_x < touches[1].pageX) && 
+					(touch_object_y < touches[0].pageY) && (touch_object_y < touches[1].pageY) && 
+					((touch_object_x + touch_object_width) > touches[0].pageX) && ((touch_object_x + touch_object_width) > touches[1].pageX) && 
+					((touch_object_y + touch_object_height) > touches[0].pageY) && ((touch_object_y + touch_object_height) > touches[1].pageY) 
+					){
+						if((touches[0].pageX > (touch_x[0]+50)) && (touches[1].pageX > (touch_x[1]+50)) &&  
+						(touches[0].pageY > touch_y[0]-30) && (touches[1].pageY > touch_y[1]-30) && 
+						(touches[0].pageY < touch_y[0]+30) && (touches[1].pageY < touch_y[1]+30)
+						){
+							if(horizontal_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeRightTwo",event);
+						}
+						if((touches[0].pageX < (touch_x[0]-50)) && (touches[1].pageX < (touch_x[1]-50)) && 
+						(touches[0].pageY > touch_y[0]-30) && (touches[1].pageY > touch_y[1]-30) && 
+						(touches[0].pageY < touch_y[0]+30) && (touches[1].pageY < touch_y[1]+30)
+						){
+							if(horizontal_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeLeftTwo",event);
+						}
+						if(
+						(touches[0].pageY > (touch_y[0]+50)) && (touches[1].pageY > (touch_y[1]+50)) && 
+						(touches[0].pageX > touch_x[0]-30) && (touches[1].pageX > touch_x[1]-30) && 
+						(touches[0].pageX < touch_x[0]+30) && (touches[1].pageX < touch_x[1]+30)
+						){
+							if(vertical_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeDownTwo",event);
+						}
+						if(
+						(touches[0].pageY < (touch_y[0]-50)) && (touches[1].pageY < (touch_y[1]-50)) && 
+						(touches[0].pageX > touch_x[0]-30) && (touches[1].pageX > touch_x[1]-30) && 
+						(touches[0].pageX < touch_x[0]+30) && (touches[1].pageX < touch_x[1]+30)
+						){
+							if(vertical_scrolled){
+								return;
+							}
+							is_swipe=false;
+							triggercustomevent(this_object,"swipeUpTwo",event);
+						}
+					}
+				}else{
+					is_swipe=false;
+				}
+			});
+			this_jq.bind("touchend",function(event){
+				is_swipe=false;
+			});
+		},
+		teardown:function(){
+			$(this).unbind("touchstart").unbind("touchmove").unbind("touchend");
+		}
+	};
+	$.event.special.scrollEvents={
+		setup:function(){
+			$(this).bind("touchmove",function(event){
+				if($(this).outerHeight()<this.scrollHeight || $(this).outerWidth()<this.scrollWidth){
+					triggercustomevent(this,"scroll",event);
+				}
+			});
+		},
+		teardown:function(){
+			$(this).unbind("touchmove");
+		}
+	};
+	$.event.special.keyboardEvents={
+		setup:function(){
+			var fire_event=false,is_focused=false,is_shown=false;
+			if(this===window){
+				fire_event=true;
+			}else{
+				fire_event=false;
+			}
+			$("input,textarea").bind("focus",function(event){
+				if(fire_event && !is_shown){
+					triggercustomevent(window,"keyboardShown",event);
+				}
+				is_focused=true;
+				is_shown=true;
+			});
+			$("input,textarea").bind("blur",function(event){
+				setTimeout(function(){
+					if($(':focus').length && ($(':focus').prop("tagName").toLowerCase()==="input" || $(':focus').prop("tagName").toLowerCase()==="textarea")){
+						is_focused=true;
+					}else{
+						is_focused=false;
+					}
+					if(fire_event && !is_focused){
+						is_shown=false;
+						triggercustomevent(window,"keyboardHidden",event);
+					}
+				},100);
+			});
+		},
+		teardown:function(){
+			$("input,textarea").unbind("focus").unbind("blur");
+		}
+	};
+	$.each(("tap tapTwo tapHold "+
+	"swipeRight swipeRightTwo "+
+	"swipeLeft swipeLeftTwo "+
+	"swipeDown swipeDownTwo "+
+	"swipeUp swipeUpTwo scroll "+
+	"orientationChange portrait landscape portraitNormal portraitUpDown landscapeLeft landscapeRight "+
+	"urlChange keyboardShown keyboardHidden "
+	).split(" "),function(i,name){
+		$.fn[name]=function(fn){
+			return fn ? this.bind(name,fn) : this.trigger(name);
+		};
+		if($.attrFn){
+			$.attrFn[name]=true;
+		}
+	});
+	$.each({
+		tap:"tapEvents",
+		tapTwo:"tapEvents",
+		tapHold:"tapEvents",
+		swipeRight:"swipeEvents",
+		swipeRightTwo:"swipeEvents",
+		swipeLeft:"swipeEvents",
+		swipeLeftTwo:"swipeEvents",
+		swipeDown:"swipeEvents",
+		swipeDownTwo:"swipeEvents",
+		swipeUp:"swipeEvents",
+		swipeUpTwo:"swipeEvents",
+		orientationChange:"orientationEvents",
+		portrait:"orientationEvents",
+		landscape:"orientationEvents",
+		portraitNormal:"orientationEvents",
+		portraitUpDown:"orientationEvents",
+		landscapeLeft:"orientationEvents",
+		landscapeRight:"orientationEvents",
+		scroll:"scrollEvents",
+		keyboardShown:"keyboardEvents",
+		keyboardHidden:"keyboardEvents"
+	},function(event,sourceEvent){
+		$.event.special[event]={
+			setup:function(){
+				$(this).bind(sourceEvent,$.noop);
+			},
+			teardown:function(){
+				$(this).unbind(sourceEvent);
+			}
+		};
+	});
+	function triggercustomevent(obj,eventType,event){
+		var originalType=event.type;
+		event.type=eventType;
+		$.event.dispatch.call(obj,event);
+	}
+})();
---
+++ README.md
@@ -0,0 +1,128 @@
+
+#[A3mobile](https://a3mobile.co)
+#[Download A3mobile](https://a3mobile.co/download-A3mobile)
+
+
+# A3mobile-events 1.0.0
+
+## urlChange
+* This event is triggered when window url changed (hash or history).
+* binded for window
+* $(window).urlChange(function(){ your code });
+* $(window ).on("urlChange",function(){ your code });
+
+## tap
+* This event is triggered after you touch screen and release finger (same point, one finger).
+* $("#element").tap(function(){ your code });
+* $(document).on("tap","#element",function(){ your code });
+
+## tapTwo
+* This event is triggered after you touch screen and release fingers (same point, two fingers).
+* $("#element").tapTwo(function(){ your code });
+* $(document).on("tapTwo","#element",function(){ your code });
+
+## tapHold
+* This event is triggered after you touch screen more than 750 ms (same point, one finger).
+* $("#element").tapHold(function(){ your code });
+* $(document).on("tapHold","#element",function(){ your code });
+
+## swipeRight
+* This event is triggered after you swipe finger more than 50 pixel to right (up 30 px, down 30 px, one finger).
+* $("#element").swipeRight(function(){ your code });
+* $(document).on("swipeRight","#element",function(){ your code });
+
+## swipeLeft
+* This event is triggered after you swipe finger more than 50 pixel to left (up 30 px, down 30 px, one finger).
+* $("#element").swipeLeft(function(){ your code });
+* $(document).on("swipeLeft","#element",function(){ your code });
+
+## swipeDown
+* This event is triggered after you swipe finger more than 50 pixel to bottom (left 30 px, right 30 px, one finger).
+* $("#element").swipeDown(function(){ your code });
+* $(document).on("swipeDown","#element",function(){ your code });
+
+## swipeUp
+* This event is triggered after you swipe finger more than 50 pixel to top (left 30 px, right 30 px, one finger).
+* $("#element").swipeUp(function(){ your code });
+* $(document).on("swipeUp","#element",function(){ your code });
+
+## swipeRightTwo
+* This event is triggered after you swipe fingers more than 50 pixel to right (up 30 px, down 30 px , two fingers).
+* $("#element").swipeRightTwo(function(){ your code });
+* $(document).on("swipeRightTwo","#element",function(){ your code });
+
+## swipeLeftTwo
+* This event is triggered after you swipe fingers more than 50 pixel to left (up 30 px, down 30 px, two fingers).
+* $("#element").swipeLeftTwo(function(){ your code });
+* $(document).on("swipeLeftTwo","#element",function(){ your code });
+
+## swipeDownTwo
+* This event is triggered after you swipe fingers more than 50 pixel to bottom (left 30 px, right 30 px, two fingers).
+* $("#element").swipeDownTwo(function(){ your code });
+* $(document).on("swipeDownTwo","#element",function(){ your code });
+
+## swipeUpTwo
+* This event is triggered after you swipe fingers more than 50 pixel to top (left 30 px, right 30 px, two fingers).
+* $("#element").swipeUpTwo(function(){ your code });
+* $(document).on("swipeUpTwo","#element",function(){ your code });
+
+## scroll
+* This event is triggered when scroll element.
+* $("#element").scroll(function(){ your code });
+* $(document).on("scroll","#element",function(){ your code });
+
+## orientationChange
+* This event is triggered when window orientation change.
+* binded for window
+* $(window).orientationChange(function(){ your code });
+* $(window ).on("orientationChange",function(){ your code });
+
+## portrait
+* This event is triggered when window orientation change to portrait mode (normal or upsidedown).
+* binded for window
+* $(window).portrait(function(){ your code });
+* $(window ).on("portrait",function(){ your code });
+
+## landscape
+* This event is triggered when window orientation change to landscape mode (left or right).
+* binded for window
+* $(window).landscape(function(){ your code });
+* $(window ).on("landscape",function(){ your code });
+
+## portraitNormal
+* This event is triggered when window orientation change to portrait mode (normal).
+* binded for window
+* $(window).portraitNormal(function(){ your code });
+* $(window ).on("portraitNormal",function(){ your code });
+
+## portraitUpDown
+* This event is triggered when window orientation change to portrait mode (upsidedown).
+* binded for window
+* $(window).portraitUpDown(function(){ your code });
+* $(window ).on("portraitUpDown",function(){ your code });
+
+## landscapeLeft
+* This event is triggered when window orientation change to landscape mode (left).
+* binded for window
+* $(window).landscapeLeft(function(){ your code });
+* $(window ).on("landscapeLeft",function(){ your code });
+
+## landscapeRight
+* This event is triggered when window orientation change to landscape mode (right).
+* binded for window
+* $(window).landscapeRight(function(){ your code });
+* $(window ).on("landscapeRight",function(){ your code });
+
+## keyboardShown
+* This event is triggered when device keyboard is shown.
+* binded for window
+* $(window).keyboardShown(function(){ your code });
+* $(window ).on("keyboardShown",function(){ your code });
+
+## keyboardShown
+* This event is triggered when device keyboard is hidden.
+* binded for window
+* $(window).keyboardHidden(function(){ your code });
+* $(window ).on("keyboardHidden",function(){ your code });
+
+
